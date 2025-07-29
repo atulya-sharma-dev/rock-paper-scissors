2360e9378b5f18210a90ea146e26c8a1f0c47273
atulya-sharma-dev <sharmaatulya2212@gmail.com>
2025-07-30 00:51:07 +0530

    Add files via upload

---
+++ Rock paper scissors game.py
@@ -0,0 +1,27 @@
+import random
+
+def rock_paper_scissors():
+    print("Welcome to Rock, Paper, Scissors!")
+
+    choices = ["rock", "paper", "scissors"]
+    player_choice = input("Enter your choice (rock,paper,scissors): ").lower()
+
+    if player_choice not in choices:
+        print("Invalid choice. Please try again.")
+        return
+
+    computer_choice == random.choice(choices)
+    print(f"The computer chose {computer_choice}.")
+
+    if player_choice == computer_choice:
+        print("It's a tie!")
+    elif (player_choice == "rock" and computer_choice == "scissors") or \
+         (player_choice == "paper" and computer_choice == "rock") or \
+         (player_choice == "scissors" and computer_choice == "paper"):
+        print("You Win!")
+
+    else:
+        print("You Lose!")
+
+rock_paper_scissors()
+         
